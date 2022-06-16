7e3146233a7cde99ec5bb26b723a440f385c36f0
Hodge <45325825+hodgejiang@users.noreply.github.com>
2022-06-16 23:10:24 +0800

    06-16

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,35 @@
 <template>
   <div>
     我是首页
+    <!-- 三级联动全局组件：三级联动已经注册为全局组件，不需要再引入 -->
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <Recommend></Recommend>
+    <Rank/>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
+import ListContainer from '@/pages/Home/ListContainer'
+import Recommend from '@/pages/Home/Recommend'
+import Rank from '@/pages/Home/Rank'
+import Like from '@/pages/Home/Like'
+import Floor from '@/pages/Home/Floor'
+import Brand from '@/pages/Home/Brand'
 export default {
-    name:'Home'
-
+    name:'Home',
+    components:{
+      ListContainer,
+      Recommend,
+      Rank,
+      Like,
+      Floor,
+      Brand
+    }
 }
 </script>
 
